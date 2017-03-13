1408c22f673201052d4d5b34a228f0e8e570f0c0
Blakeh36 <blake.holman17@gmail.com>
2017-03-13 13:16:52 -0500

    added comments to improve readability

--- eulerNum.py
+++ eulerNum.py
@@ -1,12 +1,13 @@
-def factorial(x):
+# e is approximated using sum(1/n!)
+def factorial(x):   # this function carries out the factorial
     i = x
-    while i <= x and i > 1:
+    while i <= x and i > 1: #i needs to match x so that it can count down and be multiplied each time
         x = x * (i-1)
         i -= 1
     return x
-j = 1
-e  = 1
+j = 1 #j is how many times the series will be run. In the future I'd like to instead ask the user how accurate they want it to be
+e  = 1.0
 while j < 1000:
     e = e + 1.0/factorial(float(j))
     j += 1
-print(e)
+print(e) #prints Eulers number
